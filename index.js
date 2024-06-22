1e5f9674a31be2404c5a6a747d28cd5f01402948
Fatih Durdu <mfdurdu@gmail.com>
2024-06-23 01:02:20 +0300

    some changes for cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // app.js
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 
@@ -10,13 +11,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+app.use(cors());
 
 const authRoutes = require('./routes/authRoutes');
 const listingRoutes = require('./routes/listingRoutes');
